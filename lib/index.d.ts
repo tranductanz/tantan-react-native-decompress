f9ab845016d53642ba70f712999c69eb320bc1d3
Trần Đức Tân <tantran1610@gmail.com>
2025-05-14 14:43:04 +0700

    update version 0.1.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,5 @@
+import decompressFromIndexPage from './decompressFromIndexPage';
 export declare function helloFunction(key: string): string;
 export declare function decompress(inputPath: string, outputPath: string): Promise<boolean>;
+export { decompressFromIndexPage };
 export { default as NativeBrotliCompress } from './NativeBrotliCompress';
